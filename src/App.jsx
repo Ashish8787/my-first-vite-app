5a5a1e8be0d01cbb09a3a26416c7696b09998b73
TJAshish <ashish@technojade.com>
2025-07-25 09:08:34 +0530

    create Counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import IntroCard from "./components/IntroCard";
 import UserCard from "./components/UserCard";
 import BlogCard from "./components/BlogCard";
+import Counter from "./components/Counter";
 
 const blogs = [
   {
@@ -32,11 +33,12 @@ const stdDtails = [
 function App() {
   return (
     <>
+    <Counter count={10} />
     {/* <IntroCard /> */}
-    <div style={{ textAlign: "center", marginTop: "5rem" }}>
+    {/* <div style={{ textAlign: "center", marginTop: "5rem" }}>
       <h1 style={{ color: "red" , fontSize: "3rem", fontWeight: "bold" , marginBottom: "1rem" , textTransform: "uppercase" , letterSpacing: "0.2rem" , textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)", textAlign: "center", alignItems: "center"}}>Hello React + Vite ⚡</h1>
       <p>Day 1: I just started learning React!</p>
-    </div>
+    </div> */}
     {/* <UserCard name="John Doe" email="john.doe@example.com" phone="123-456-7890" />
     <UserCard name="Jane Doe" email="jane.doe@example.com" phone="123-456-7890" />
     <UserCard name="Jim Doe" email="jim.doe@example.com" phone="123-456-7890" /> */}
